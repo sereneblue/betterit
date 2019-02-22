027b50e176d6ff3ec80317c148b4773f8ec64b61
sereneblue <sereneblue@users.noreply.github.com>
2019-02-21 22:39:55 -0500

    refactor vuex store

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Listing from "./views/Listing.vue";
 import Thread from "./views/Thread.vue";
+import store from "./store";
 
 Vue.use(Router);
 
@@ -10,18 +11,26 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Listing
+      redirect: '/r/popular'
     },
     {
       path: "/r/:subreddit/:sort?",
       name: "subreddit",
       component: Listing,
+      beforeEnter: (to, from, next) => {
+        store.dispatch('clearState');
+        next();
+      },
       props: true
     },
     {
       path: "/r/:subreddit/t/:id",
       name: "thread",
       component: Thread,
+      beforeEnter: (to, from, next) => {
+        store.dispatch('clearState');
+        next();
+      },
       props: true
     }
   ]
--- src/store.js
+++ src/store.js
@@ -7,70 +7,80 @@ export default new Vuex.Store({
   state: {
     comments: [],
     error: "",
+    listings: [],
+    listingsLoaded: false,
     subreddit: "",
     thread: {},
-    threads: []
+    threadLoaded: false
   },
   mutations: {
-    clear: (state) => {
-        state.comments.length = 0;
-        state.threads.length = 0;
-        state.thread = Object.assign(state.thread, {});
+    CLEAR: (state) => {
+      state.comments.length = 0;
+      state.listings.length = 0;
+      state.thread = Object.assign(state.thread, {});
     },
-    updateComments: (state, comment) => {
-        state.comments.push(comment);
+    LISTINGS_LOADED: (state) => {
+      Vue.set(state, 'listingsLoaded', true);
     },
-    updateError: (state, err) => {
-        state.error = err;
+    THREAD_LOADED: (state) => {
+      Vue.set(state, 'threadLoaded', true);
     },
-    updateSubreddit: (state, sub) => {
-        state.subreddit = sub
+    UPDATE_COMMENTS: (state, comments) => {
+      Vue.set(state, 'comments', comments);
     },
-    updateThread: (state, thread) => {
-        state.thread = Object.assign(state.thread, thread);
+    UPDATE_ERROR: (state, err) => {
+      Vue.set(state, 'err', err);
     },
-    updateThreads: (state, thread) => {
-        state.threads.push(thread);
+    UPDATE_LISTINGS: (state, listings) => {
+      Vue.set(state, 'listings', listings);
+    },
+    UPDATE_SUBREDDIT: (state, sub) => {
+      Vue.set(state, 'subreddit', sub);
+    },
+    UPDATE_THREAD: (state, thread) => {
+      Vue.set(state, 'thread', thread);
     }
   },
   actions: {
+    clearState: ({commit, state}) => {
+      commit('CLEAR');
+    },
     getListings: async ({ commit, state }, params ) => {
-        let sub = params.subreddit ? params.subreddit : "popular";
-        let order = params.sort ? "/" + params.sort : "";
+      let sub = params.subreddit ? params.subreddit : "popular";
+      let order = params.sort ? "/" + params.sort : "";
 
-        let res = await fetch(`https://www.reddit.com/r/${sub}${order}.json`);
-        let response = await res.json();
+      let res = await fetch(`https://www.reddit.com/r/${sub}${order}.json`);
+      let response = await res.json();
 
-        if (res.status == 404) {
-          commit('updateError', response.reason ? response.reason : response.message);
-        } else {
-          commit('updateError', "");
-          commit('updateSubreddit', sub);
-          commit('clear');
-
-          for (var i = 0; i < response.data.children.length; i++) {
-            commit('updateThreads', response.data.children[i].data);
-          }
-        }
+      if (res.status == 404) {
+        commit('UPDATE_ERROR', response.reason ? response.reason : response.message);
+      } else {
+        commit('UPDATE_ERROR', "");
+        commit('UPDATE_SUBREDDIT', sub);
+        commit('UPDATE_LISTINGS', response.data.children.filter(t => {
+          return t.kind == "t3"
+        }).map(t => t.data));
+        commit('LISTINGS_LOADED');
+      }
     },
     getComments: async ({ commit, state }, params ) => {
-        let sub = params.subreddit ? params.subreddit : "popular";
+      let sub = params.subreddit ? params.subreddit : "popular";
 
-        let res = await fetch(`https://www.reddit.com/r/${sub}/comments/${params.id}.json`);
-        let response = await res.json();
+      let res = await fetch(`https://www.reddit.com/r/${sub}/comments/${params.id}.json`);
+      let response = await res.json();
 
-        if (res.status == 404) {
-          commit('updateError', response.reason ? response.reason : response.message);
-        } else {
-          commit('updateError', "");
-          commit('updateSubreddit', sub);
-          commit('clear');
+      if (res.status == 404) {
+        commit('UPDATE_ERROR', response.reason ? response.reason : response.message);
+      } else {
+        commit('UPDATE_ERROR', "");
+        commit('UPDATE_SUBREDDIT', sub);
 
-          commit('updateThread', response[0].data.children[0].data);
-          for (var i = 0; i < response[1].data.children.length; i++) {
-            commit('updateComments', response[1].data.children[i]);
-          }
-        }
+        commit('UPDATE_COMMENTS', response[1].data.children.filter(c => {
+          return c.kind == "t1"
+        }).map(c => c.data));
+        commit('UPDATE_THREAD', response[0].data.children[0].data);
+        commit('THREAD_LOADED');
+      }
     }
   }
 });
